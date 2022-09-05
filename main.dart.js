c412c5d423db2a3590ec9f7601f3a0c631d6ebf9
Kaan <erolkaan92@gmail.com>
2022-09-05 03:21:33 +0300

    reworked

--- main.dart.js
+++ main.dart.js
@@ -62255,43 +62255,43 @@ a(b.target)}for(var r=0;r<s.length;++r)s[r].addEventListener("load",onLoad,false
 var s=A.a57
 if(typeof dartMainRunner==="function")dartMainRunner(s,[])
 else s([])})})()
-(function() {
-    var supportsPassive = eventListenerOptionsSupported();  
+// (function() {
+//     var supportsPassive = eventListenerOptionsSupported();  
   
-    if (supportsPassive) {
-      var addEvent = EventTarget.prototype.addEventListener;
-      overwriteAddEvent(addEvent);
-    }
+//     if (supportsPassive) {
+//       var addEvent = EventTarget.prototype.addEventListener;
+//       overwriteAddEvent(addEvent);
+//     }
   
-    function overwriteAddEvent(superMethod) {
-      var defaultOptions = {
-        passive: true,
-        capture: false
-      };
+//     function overwriteAddEvent(superMethod) {
+//       var defaultOptions = {
+//         passive: true,
+//         capture: false
+//       };
   
-      EventTarget.prototype.addEventListener = function(type, listener, options) {
-        var usesListenerOptions = typeof options === 'object';
-        var useCapture = usesListenerOptions ? options.capture : options;
+//       EventTarget.prototype.addEventListener = function(type, listener, options) {
+//         var usesListenerOptions = typeof options === 'object';
+//         var useCapture = usesListenerOptions ? options.capture : options;
   
-        options = usesListenerOptions ? options : {};
-        options.passive = options.passive !== undefined ? options.passive : defaultOptions.passive;
-        options.capture = useCapture !== undefined ? useCapture : defaultOptions.capture;
+//         options = usesListenerOptions ? options : {};
+//         options.passive = options.passive !== undefined ? options.passive : defaultOptions.passive;
+//         options.capture = useCapture !== undefined ? useCapture : defaultOptions.capture;
   
-        superMethod.call(this, type, listener, options);
-      };
-    }
+//         superMethod.call(this, type, listener, options);
+//       };
+//     }
   
-    function eventListenerOptionsSupported() {
-      var supported = false;
-      try {
-        var opts = Object.defineProperty({}, 'passive', {
-          get: function() {
-            supported = true;
-          }
-        });
-        window.addEventListener("test", null, opts);
-      } catch (e) {}
+//     function eventListenerOptionsSupported() {
+//       var supported = false;
+//       try {
+//         var opts = Object.defineProperty({}, 'passive', {
+//           get: function() {
+//             supported = true;
+//           }
+//         });
+//         window.addEventListener("test", null, opts);
+//       } catch (e) {}
   
-      return supported;
-    }
-  }());
+//       return supported;
+//     }
+//   }());
